37629ed4578170615b7556cab85a30308711b8c7
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2021-08-05 12:34:30 +0200

    Fix accepted args

--- lib/gradualizer_ex/elixir_file_utils.ex
+++ lib/gradualizer_ex/elixir_file_utils.ex
@@ -8,21 +8,24 @@ defmodule GradualizerEx.ElixirFileUtils do
   @doc """
   Accepts a filename or the beam code as a binary
   """
-  @spec get_forms_from_beam(:file.filename() | binary()) ::
+  @spec get_forms_from_beam(:file.filename() | String.t()) ::
           {:ok, [SpecifyErlAst.form()]} | {:error, any()}
-  def get_forms_from_beam(file) do
-    case :beam_lib.chunks(file, [:abstract_code]) do
+  def get_forms_from_beam(path) when is_binary(path),
+    do: get_forms_from_beam(String.to_charlist(path))
+
+  def get_forms_from_beam(path) do
+    case :beam_lib.chunks(path, [:abstract_code]) do
       {:ok, {_module, [{:abstract_code, {:raw_abstract_v1, forms}}]}} ->
         {:ok, forms |> SpecifyErlAst.specify()}
 
       {:ok, {_module, [{:abstract_code, :no_abstract_code}]}} ->
-        {:forms_not_found, file}
+        {:forms_not_found, path}
 
       {:error, :beam_lib, {:file_error, _, :enoent}} ->
-        {:file_not_found, file}
+        {:file_not_found, path}
 
       {:error, :beam_lib, {:file_error, _, reason}} ->
-        {:file_open_error, {reason, file}}
+        {:file_open_error, {reason, path}}
 
       {:error, :beam_lib, reason} ->
         {:forms_error, reason}
