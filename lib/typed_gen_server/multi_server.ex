19ee60348be9aa967871883d914a63979fc2be82
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-05 00:27:49 +0200

    wip

--- lib/typed_gen_server/multi_server.ex
+++ lib/typed_gen_server/multi_server.ex
@@ -1,12 +1,22 @@
 defmodule TypedGenServer.MultiServer do
   use GenServer
 
-  @type protocol :: Proto.Echo.req() | Proto.Hello.req()
-  # @type protocol :: {:echo_req, String.t()} | {:hello, String.t()}
+  @type message :: Proto.Echo.req() | Proto.Hello.req()
+  # @type message :: {:echo_req, String.t()} | {:hello, String.t()}
   @type state :: map()
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, %{})
+  end
+
+  @spec echo(pid(), String.t()) :: Proto.Echo.res()
+  def echo(pid, message) do
+    case GenServer.call(pid, {:echo_req, message})
+  end
+
+  @spec hello(pid, String.t()) :: Proto.Hello.res()
+  def hello(pid, name) do
+    GenServer.call(pid, {:hello, name})
   end
 
   @impl true
@@ -19,15 +29,15 @@ defmodule TypedGenServer.MultiServer do
     {:noreply, handle(m, from, state)}
   end
 
-  @spec handle(protocol(), any, any) :: state()
-  def handle({:echo_reqz, payload}, from, state) do
+  @spec handle(message(), any, any) :: state()
+  def handle({:echo_req, payload}, from, state) do
     GenServer.reply(from, {:echo_res, payload})
     state
   end
 
-  # def handle({:hello, name}, from, state) do
-  #  IO.puts("Hello, #{name}!")
-  #  GenServer.reply(from, :ok)
-  #  state
-  # end
+  def handle({:hello, name}, from, state) do
+    IO.puts("Hello, #{name}!")
+    GenServer.reply(from, :ok)
+    state
+  end
 end
