6812bcbd604cd3123b13f68dce2506b36f0b2cd2
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-07 09:44:42 +0200

    wip: Save the mess

--- lib/typed_gen_server/multi_server.ex
+++ lib/typed_gen_server/multi_server.ex
@@ -1,51 +1,79 @@
 defmodule TypedGenServer.MultiServer do
-  use GenServer
+  #use GenServer
   use GradualizerEx.TypeAnnotation
 
   ## recompile(); GradualizerEx.type_check_file(:code.which(TypedGenServer.MultiServer), [:infer])
 
-  @type message :: Proto.Echo.req() | Proto.Hello.req()
-  # @type message :: {:echo_req, String.t()} | {:hello, String.t()}
-  @type state :: map()
+  #@type message :: Proto.Echo.req() | Proto.Hello.req()
+  #@type message :: {:echo_req, String.t()} | {:hello, String.t()}
+  #@type state :: map()
 
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, %{})
-  end
+  #def start_link(_) do
+  #  GenServer.start_link(__MODULE__, %{})
+  #end
 
-  @spec echo(pid(), String.t()) :: Proto.Echo.res()
+  ##@spec echo() :: :ok
+  #@spec echo() :: Proto.Echo.res()
+  #def echo() do
+  #  #:ok
+  #  #annotate_type( GenServer.call(:pid, :ok), Proto.Echo.res() )
+  #  #annotate_type( GenServer.call(:pid, :ok), list() )
+  #  #assert_type( GenServer.call(:pid, :ok), :ok)
+  #  assert_type( GenServer.call(:pid, :ok), Proto.Echo.res() )
+  #end
+
+  #def test() do
+  #  [
+  #    quote do
+  #    "asd"
+  #    end,
+  #    quote do
+  #      'asd'
+  #    end,
+  #    quote do
+  #      :'asd'
+  #    end,
+  #    quote do
+  #      {:string, 0, 'asd'}
+  #    end,
+  #    unquote({:{}, [], [:string, 0, 'asd']})
+  #  ] |> Enum.each(&IO.inspect(&1))
+  #end
+
+  @spec echo(pid(), String.t()) :: String.t()
+  #@spec echo(pid(), String.t()) :: {:echo_req, String.t()}
   def echo(pid, message) do
-    case annotate_type(
-           GenServer.call(pid, {:echo_req, message}),
-           Proto.Echo.res()
-         ) do
-      {:echo_ressdasdz, ^message} -> message
+    #annotated = assert_type( GenServer.call(pid, {:echo_req, message}), Proto.Echo.res() )
+    #annotated = 
+    case annotate_type( GenServer.call(pid, {:echo_req, message}), Proto.Echo.res() ) do
+      {:echo_res, response} -> response
     end
   end
 
-  @spec hello(pid, String.t()) :: Proto.Hello.res()
-  def hello(pid, name) do
-    GenServer.call(pid, {:hello, name})
-  end
+  #@spec hello(pid, String.t()) :: Proto.Hello.res()
+  #def hello(pid, name) do
+  #  GenServer.call(pid, {:hello, name})
+  #end
 
-  @impl true
-  def init(state) do
-    {:ok, state}
-  end
+  #@impl true
+  #def init(state) do
+  #  {:ok, state}
+  #end
 
-  @impl true
-  def handle_call(m, from, state) do
-    {:noreply, handle(m, from, state)}
-  end
+  #@impl true
+  #def handle_call(m, from, state) do
+  #  {:noreply, handle(m, from, state)}
+  #end
 
-  @spec handle(message(), any, any) :: state()
-  def handle({:echo_req, payload}, from, state) do
-    GenServer.reply(from, {:echo_res, payload})
-    state
-  end
+  #@spec handle(message(), any, any) :: state()
+  #def handle({:echo_req, payload}, from, state) do
+  #  GenServer.reply(from, {:echo_res, payload})
+  #  state
+  #end
 
-  def handle({:hello, name}, from, state) do
-    IO.puts("Hello, #{name}!")
-    GenServer.reply(from, :ok)
-    state
-  end
+  #def handle({:hello, name}, from, state) do
+  #  IO.puts("Hello, #{name}!")
+  #  GenServer.reply(from, :ok)
+  #  state
+  #end
 end
