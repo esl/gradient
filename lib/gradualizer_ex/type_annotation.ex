4035f7b4b4689ec20966ed80efaed6ee6c703d53
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-07 08:17:19 +0200

    Translate Elixir types to Erlang types before passing to Gradualizer

--- lib/gradualizer_ex/type_annotation.ex
+++ lib/gradualizer_ex/type_annotation.ex
@@ -1,12 +1,30 @@
 defmodule GradualizerEx.TypeAnnotation do
 
+  defmacro annotate_type(expr, type),
+    do: cast(:'::', expr, type)
 
-  defmacro annotate_type(expr, type) do
-    {:'::', [], [expr, Macro.to_string(type)]}
+  defmacro assert_type(expr, type),
+    do: cast(:':::', expr, type)
+
+  defp cast(type_op, expr, type) do
+    erlang_type = elixir_type_to_erlang(type)
+    #IO.inspect(erlang_type, label: "erlang type")
+    {type_op, [], [expr, Macro.to_string(erlang_type)]}
+    #|> IO.inspect(label: "translate type")
   end
 
-  defmacro assert_type(expr, type) do
-    {:':::', [], [expr, Macro.to_string(type)]}
+  defp elixir_type_to_erlang(type) do
+    case type do
+      {{:., _, [{:__aliases__, _, path}, name]}, _, [] = _args} ->
+        #unquote({:{}, [], [:string, 0, '\'Elixir.Fake\':t()']})
+        #{:string, 0, Macro.escape("'Elixir.#{Enum.join(path, ".")}':#{name}()" |> to_charlist())}
+        "'Elixir.#{Enum.join(path, ".")}':#{name}()"
+      _ when is_atom(type) ->
+        Atom.to_string(type)
+      other ->
+        #unquote({:{}, [], [:string, 0, '\'Elixir.Fake\':t()']})
+        other
+    end
   end
   
   defmacro __using__(_) do
