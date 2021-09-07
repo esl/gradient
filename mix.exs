9ddc9938782a12d4466827821af8992b944c348b
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-09-07 10:29:07 +0200

    Add Dialyzer

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule TypedGenServer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -24,9 +25,24 @@ defmodule TypedGenServer.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:gradualizer,
        github: "erszcz/Gradualizer", ref: "typed-gen-server", manager: :rebar3, override: true},
       {:gradualizer_ex, github: "erszcz/gradualizer-ex", branch: "rs/wip"}
     ]
   end
+
+  defp dialyzer do
+    [
+      plt_add_deps: :app_tree,
+      #ignore_warnings: "dialyzer.ignore-warnings",
+      flags: ~w(
+        error_handling
+        race_conditions
+        unmatched_returns
+        underspecs
+      )a
+    ]
+  end
+
 end
