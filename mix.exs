2a76368a0c8cc3d9adeac5cba112c9b92912438e
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-10-18 18:12:55 +0200

    Don't test on Elixir 1.11 for now

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Gradient.MixProject do
     [
       app: :gradient,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
