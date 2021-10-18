f18113f381545cfcc239c5bb12f1b9b9d8573961
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-10-18 18:31:52 +0200

    Add SimpleApp.StructExample

---
+++ examples/simple_app/lib/simple_app/struct_example.ex
@@ -0,0 +1,16 @@
+defmodule SimpleApp.StructExample do
+  defmodule SomeStruct do
+    defstruct [name: "John Smith", age: 25]
+  end
+
+  @type t :: %SomeStruct{}
+
+  @spec age(t) :: non_neg_integer
+  def age(%SomeStruct{age: age}), do: age
+
+  @spec mistake :: :ok
+  def mistake do
+    age(%{first: "John", last: "Smith", age: 32})
+  end
+
+end
