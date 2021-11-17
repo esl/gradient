c4f5d0230b850e82b25b687aef1836ed668808e1
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2021-11-17 12:44:33 +0100

    Add docs

--- lib/gradient/debug.ex
+++ lib/gradient/debug.ex
@@ -1,16 +1,34 @@
 defmodule Gradient.Debug do
-  @moduledoc false
+  @moduledoc ~S"""
+  Helpers for convenient debugging Erlang and Elixir ASTs.
+  """
 
+  ## TODO: specify elixir_form
+  @type elixir_form() :: any()
+  @type erlang_form() :: Gradient.SpecifyErlAst.form()
+
+  @doc ~S"""
+  Return the Elixir AST of an Elixir module.
+  """
+  @spec elixir_ast(module()) :: {:ok, [elixir_form()]}
   def elixir_ast(mod) do
     {:ok, {_, [{:debug_info, {:debug_info_v1, :elixir_erl, abstract_code}}]}} = 
       :beam_lib.chunks(get_beam_path_as_charlist(mod), [:debug_info])
     {:ok, _forms} = :elixir_erl.debug_info(:elixir_v1, :module_name, abstract_code, [])
   end
 
+  @doc ~S"""
+  Return the Erlang AST of an Erlang or Elixir module.
+  """
+  @spec erlang_ast(module()) :: {:ok, [erlang_form()]}
   def erlang_ast(mod) do
     {:ok, _forms} = get_beam_path_as_charlist(mod) |> Gradient.ElixirFileUtils.get_forms_from_beam()
   end
 
+  @doc ~S"""
+  Print module as Erlang source code.
+  """
+  @spec print_erlang(module()) :: :ok
   def print_erlang(mod) do
     {:ok, forms} = erlang_ast(mod)
     IO.puts(:erl_prettypr.format(:erl_syntax.form_list(forms)))
