8b02569e96896ef45f5cef546a7ba283797bb2f7
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2022-03-04 14:32:26 +0100

    Collect options and provide descriptions

--- lib/mix/tasks/gradient.ex
+++ lib/mix/tasks/gradient.ex
@@ -1,8 +1,61 @@
 defmodule Mix.Tasks.Gradient do
+  @moduledoc ~s"""
+  This task compiles the mix project, collects files with dependencies, specify ast tree,
+  and run Elixir checks and Gradualizer.
+
+  ## Command-line options
+
+    * `--no-compile` - do not compile even if needed
+    * `--no-ex-check` - do not perform checks specyfic for Elixir
+      (from ElixirChecker module)
+    * `--no-gradualizer-check` - do not perform Gradualizer checks
+
+    * `--stop_on_first_error` - stop type checking at the first error
+    * `--infer` - infer type information from literals and other language
+      constructs,
+    * `--verbose` - show what Gradient is doing
+    * `--no-fancy` - do not use fancy error messages
+    * `--fmt-location none` - do not display location for easier comparison
+    * `--fmt-location brief` - display location for machine processing 
+    * `--fmt-location verbose` - display location for human readers (default)
+
+    * `--no-colors` - do not use colors in printed messages
+    * `--expr-color ansicode` - set color for expressions (default: cyan)
+    * `--type-color ansicode` - set color for types
+    * `--underscore-color ansicode` - set color for the underscored invalid code part
+      in the fancy messages
+
+  Warning flags passed to this task are passed on to :gradualizer.
+  """
+  @shortdoc "Runs gradient with default or project-defined flags"
+
   use Mix.Task
 
+  @options [
+    # skip phases options
+    no_compile: :boolean,
+    no_ex_check: :boolean,
+    no_gradualizer_check: :boolean,
+    no_specify: :boolean,
+    # checker options
+    stop_on_first_error: :boolean,
+    infer: :boolean,
+    # formatter options
+    verbose: :boolean,
+    no_fancy: :boolean,
+    fmt_location: :string,
+    quiet: :boolean,
+    # colors options
+    no_colors: :boolean,
+    expr_color: :string,
+    type_color: :string,
+    underscore_color: :string
+  ]
+
   @impl Mix.Task
-  def run(_args) do
+  def run(args) do
+    IO.inspect(args, label: "Args")
+    {parsed, paths, _invalid} = OptionParser.parse(args, strict: @options)
     # Compile the project before the analysis
     Mix.Tasks.Compile.run([])
 
