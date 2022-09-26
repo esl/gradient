c9b8860aa55c55d2db39dfa355ed22061de5409d
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-09-26 20:18:10 +0200

    Update Gradualizer
    Changes from the previous version - the main one is getting rid of
    (definitely most, possibly all) infinite loops:
    
      $ git l --merges
      ba5481c 2022-09-26 Merge pull request #463 from erszcz/fix-integer-user-type-alias
      c45a82e 2022-09-24 Merge pull request #459 from erszcz/install-asdf
      f305847 2022-09-24 Merge pull request #460 from erszcz/housekeeping
      9493c66 2022-09-23 Merge pull request #458 from erszcz/fix-more-infinite-loops
      e874016 2022-09-19 Merge pull request #457 from erszcz/fix-map-type-inference

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Gradient.MixProject do
   # Run "mix help deps" to learn about dependencies.
   def deps do
     [
-      {:gradualizer, github: "josefs/Gradualizer", ref: "e874016", manager: :rebar3},
+      {:gradualizer, github: "josefs/Gradualizer", ref: "ba5481c", manager: :rebar3},
       # {:gradualizer, path: "../Gradualizer/", manager: :rebar3},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.28.4", only: [:dev], runtime: false}
