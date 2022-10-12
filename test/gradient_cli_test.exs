bdfc45f3fef05d2c4716d356dd999bc19ee7c530
zofpolkowska <zofia.polkowska@erlang-solutions.com>
2022-10-12 11:05:19 +0200

    Verify that the asdf paths are added

---
+++ test/gradient_cli_test.exs
@@ -0,0 +1,13 @@
+
+defmodule Gradient.CLITest do
+  use ExUnit.Case
+
+  test "--path-add option" do
+    {_, code} =
+      System.cmd("sh", ["-c", "./gradient test/examples/1.12/range_step.ex"]) |> IO.inspect()
+
+    # {_, code} = System.cmd("./gradient", ["test/examples/1.12/range_step.ex"])
+
+    assert 0 == code
+  end
+end
