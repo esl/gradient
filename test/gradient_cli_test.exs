497a1ba48c027ba3a259841f6b6eeb315b2dd715
zofpolkowska <zofia.polkowska@erlang-solutions.com>
2022-10-14 14:42:21 +0200

    Exclude escript test in the main workflow

--- test/gradient_cli_test.exs
+++ test/gradient_cli_test.exs
@@ -1,11 +1,9 @@
 defmodule Gradient.CLITest do
   use ExUnit.Case
 
+  @tag :requires_asdf
   test "--path-add option" do
-    {_, code} =
-      System.cmd("sh", ["-c", "./gradient test/examples/1.12/range_step.ex"]) |> IO.inspect()
-
-    # {_, code} = System.cmd("./gradient", ["test/examples/1.12/range_step.ex"])
+    {_, code} = System.cmd("./gradient", ["test/examples/1.12/range_step.ex"])
 
     assert 0 == code
   end
