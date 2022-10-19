90242265ebd6309dc90f47e86ead88ca6d46ccc3
zofpolkowska <zofia.polkowska@erlang-solutions.com>
2022-10-19 10:43:48 +0200

    Try adding to .gradient_ignore.exs all of the errors that gradient returns

--- .gradient_ignore.exs
+++ .gradient_ignore.exs
@@ -4,6 +4,18 @@
   # ?
   {"lib/gradient/elixir_type.ex:70", {:type_error, :mismatch}},
 
+  "lib/gradient/elixir_type.ex:38",
+
+  "lib/gradient/debug.ex:58",
+
+  "lib/gradient/ast_specifier.ex:436",
+
+  "lib/gradient/elixir_fmt.ex:215",
+
+  "lib/gradient.ex:77",
+
+  "lib/gradient.ex:184",
+
   # https://github.com/esl/gradient/issues/85
   {:type_error, :unreachable_clause},
 
