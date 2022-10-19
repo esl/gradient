2853ca168fba31d550fe0c30cf7a05dde2801f7f
Zofia Polkowska <33136024+zofpolkowska@users.noreply.github.com>
2022-10-19 09:06:46 +0200

    Update test/gradient_cli_test.exs asdf test name
    Co-authored-by: Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>

--- test/gradient_cli_test.exs
+++ test/gradient_cli_test.exs
@@ -2,7 +2,7 @@ defmodule Gradient.CLITest do
   use ExUnit.Case
 
   @tag :requires_asdf
-  test "--path-add option" do
+  test "Gradient escript detects asdf-installed Elixir libs" do
     {_, code} = System.cmd("sh", ["-c", "./gradient test/examples/1.12/range_step.ex"])
 
     assert 0 == code
