2b14b567e107906c6058ec7d41ac9e91708d732f
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2022-12-22 11:33:26 +0100

    Rename: gradient_runtime -> gradient_macros

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Gradient.MixProject do
   def deps do
     [
       {:gradualizer, github: "josefs/Gradualizer", ref: "5567a23", manager: :rebar3},
-      {:gradient_runtime, github: "esl/gradient_runtime", ref: "ee8de90"},
+      {:gradient_macros, github: "esl/gradient_macros", ref: "bf5a066", runtime: false},
       # {:gradualizer, path: "../Gradualizer/", manager: :rebar3},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.28.4", only: [:dev], runtime: false}
